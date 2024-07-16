4d5924312abbcebc88017f10b021669bd33188c2
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-16 22:54:53 +0200

    Django rest framework

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('base.urls')),
+    path('api/', include('base.urls')),
     path('admin/', admin.site.urls),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .products import products
 
+@api_view(['GET'])
 def getRotes(request):
-    return JsonResponse('Hello', safe=False)
+    return Response('hello')
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+    return Response(product)
