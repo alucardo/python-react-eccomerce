85492f5c3b1295afa6432f5a7a57c6071e57b1cd
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-16 08:20:01 +0200

    Product card

---
+++ frontend/src/products.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+        '_id': '1',
+        'name': 'Airpods Wirless',
+        'image': 'https://placehold.co/600x400',
+        'description': 'adsadada dfsdads asdasd adadsd s',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 89.99,
+        'countInStock': 10,
+        'rating': 4.5,
+        'numReviews': 12,
+    },
+    {
+        '_id': '2',
+        'name': 'Iphone 11',
+        'image': 'https://placehold.co/600x400',
+        'description': 'adsadada dfsdads asdasd adadsd s',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 189.99,
+        'countInStock': 1,
+        'rating': 3.5,
+        'numReviews': 2,
+    },
+]
+
+export default products
