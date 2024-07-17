68f5496db75cbd06dd31fb770cf9e4cb57bb66c8
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-17 22:34:34 +0200

    Serialize db data

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRotes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,20 +2,17 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .products import products
-
-@api_view(['GET'])
-def getRotes(request):
-    return Response('hello')
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-    return Response(product)
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
