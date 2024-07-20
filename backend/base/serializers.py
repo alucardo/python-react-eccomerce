c0f5b8554a2f82cdc3ca8b5eba0614f95202b740
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-20 20:00:06 +0200

    Api user registration

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
     def get_is_admin(self, obj):
         return obj.is_staff
 
-def UserSerializerWithToken(UserSerializer):
+class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = User
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/', views.getUsers, name='users'),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='users-register'),
     path('users/profile/', views.getUserProfile, name='users-profile'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product'),
