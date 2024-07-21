8d812b2d49bed660c7c76cfcb988d0e1b4db8b07
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2024-07-21 15:12:15 +0200

    Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import { LinkContainer } from 'react-router-bootstrap'
+import { logout } from "../actions/userActions"
 
 function Header() {
-  return (
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+    const dispatch = useDispatch()
+
+    const logoutHandler = (e) => {
+        dispatch(logout())
+    }
+
+    return (
       <header>
         <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="dark" collapseOnSelect>
           <Container>
@@ -14,13 +24,25 @@ function Header() {
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <LinkContainer to='/cart'><Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link></LinkContainer>
-                    <LinkContainer to='/login'><Nav.Link><i className="fas fa-user"></i> Login</Nav.Link></LinkContainer>
+
+                    {
+                        userInfo ? (
+                            <NavDropdown title={userInfo.name} id='username'>
+                                <LinkContainer to='/profile'>
+                                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                                </LinkContainer>
+                                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : (
+                            <LinkContainer to='/login'><Nav.Link><i className="fas fa-user"></i> Login</Nav.Link></LinkContainer>
+                        )
+                    }
                 </Nav>
               </Navbar.Collapse>
           </Container>
         </Navbar>
       </header>
-  );
+    );
 }
 
 export default Header;
